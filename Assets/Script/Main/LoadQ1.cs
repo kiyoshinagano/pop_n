0d77d9348336ffcc02644394dde7b22fbc3de627
MomenCyaridar <catnap.12683@gmail.com>
2016-09-20 22:27:35 +0900

    Sample1.0

---
+++ Assets/Script/Main/LoadQ1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadQ1 : MonoBehaviour {
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Q1");
+    }
+}
